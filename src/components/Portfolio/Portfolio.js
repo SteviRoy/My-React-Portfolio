2b9031599e813b1c7b41174d54fa750149f20db9
S.P. ROY <sprithi44@gmail.com>
2023-04-12 23:31:57 -0400

    Portfolio added

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Portfolio.css";
+import Project from "../Project/Project";
+import projectData from "../../data/projects.json";
+
+const Portfolio = () => {
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio-title">My Projects</h2>
+      <div className="project-list">
+        {projectData.map((project) => (
+          <Project key={project.id} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
